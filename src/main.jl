cd12a020a935398709bb461646be2e8ba5fb8fa8
Helisson Sousa <helissoncg@hotmail.com>
2025-09-05 17:55:30 -0300

    subTour

--- src/main.jl
+++ src/main.jl
@@ -14,18 +14,12 @@ function main()
     println("Instância: ", getInstanceName(data))
     println("Dimensão: ", data.dimension)
 
-    # for i in 1:data.dimension-1
-    #     for j in 1:data.dimension-1
-    #         println(data.distMatrix[i, j])
-    #     end
-    # end
-
     assignment, cost = hungarian(data.distMatrix)
 
     # Mostrar a matriz
-    println("Matriz de matching:")
-    println(assignment)
-    println("Custo", cost)
+    # println("Matriz de matching:")
+    # println(assignment)
+    # println("Custo", cost)
 
     matrix = zeros(data.dimension, data.dimension)
     # print(matrix)
@@ -34,7 +28,53 @@ function main()
         j = assignment[i]
         matrix[i, j] = 1
     end
-    print(matrix)
+    # print(matrix)
+
+    #criar vetor interno de tour
+    tour = Vector{Int64}(undef, 0)
+
+    # lista de vetores visitados
+
+    visited = Vector{Int64}(undef, 0)
+
+    #lista de vetores não visitados
+
+    notVisited = Vector{Int64}(undef, 0)
+
+    #Preenche vertices não visitados
+
+    for i in 1:data.dimension
+        push!(notVisited, i)
+    end
+
+    # print("visited", visited)
+    # print("notVisited", notVisited)
+
+    n = length(notVisited)
+
+    # while n > 0
+        i = notVisited[begin]
+        popfirst!(notVisited)
+        push!(visited, i)
+
+        print("visited", visited)
+        print("notVisited", notVisited)
+    # end
+
+    # pegue o primeiro vertice da lista de vertices não visitados
+    # retire dos não visitados
+    # adicione um vertice no tour
+    
+    # para os demais vertices :
+    #   se tiver atribuido: 
+    #     adicione  j nos vertices visitados
+    #     remova j dos não visitados
+    #     adicione no tour
+    #     fazer i = j
+
+    #   se a posição atual for igual o vertice inicial do tour
+    #     adicione o tour no subtours
+    #     limpe o tour
 
 end
 
